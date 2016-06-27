996ee8031ed102ee4fed1dc2e0d26d6c55e32eb4
hjone72 <Hayden.Jones@brisbanegrammar.com>
2016-06-27 11:45:21 +1000

    PlexAuth - SignUp

--- pages/signup.php
+++ pages/signup.php
@@ -3,18 +3,32 @@
 $username = (isset($_POST['username'])) ? htmlspecialchars($_POST['username']) : '';
 $email = (isset($_POST['email'])) ? htmlspecialchars($_POST['email']) : '';
 
-if (isset($_POST['new_user'])) {
-	$settings = new Settings();
-	$ans = $settings->new_user($_POST);
-	if ($ans === true) {
-		$_SESSION['alert'] = array('text' => Trad::A_SUCCESS_SIGNUP, 'type' => 'alert-success');
-		header('Location: '.Url::parse('home'));
-		exit;
+	if (isset($_POST['new_user'])) {
+		//This should never return false but just in case.
+		if (isset($_SESSION['ytbuser'])){
+			$User = unserialize($_SESSION['ytbuser']);
+			$auth = $User->getAuth();
+			$ClaimedUser = $User->getUsername();
+					
+			if ($auth) {
+				$_POST['username'] = (string)$User->getUsername();
+				$_POST['email'] = (string)$User->getEmail();
+				$_POST['password'] = (string)$User->getID();
+				$settings = new Settings();
+				$ans = $settings->new_user($_POST);
+				if ($ans === true) {
+					$_SESSION['alert'] = array('text' => Trad::A_SUCCESS_SIGNUP, 'type' => 'alert-success');
+					header('Location: '.Url::parse('home'));
+					exit;
+				} else {
+					$this->addAlert($ans);
+				}
+			}
+		} else {
+			header("Location: https://secure.domain.com"); //Change this to URL of PlexAuth
+			die();
+		}
 	}
-	else {
-		$this->addAlert($ans);
-	}
-}
 
 $title = Trad::V_SIGNUP;
 
@@ -23,13 +37,8 @@ $content = '
 <h1>'.Trad::V_SIGNUP.'</h1>
 
 <form action="'.Url::parse('signup').'" method="post" class="form form-signup">
-	<label for="username2">'.Trad::F_USERNAME.'</label>
-	<input type="text" name="username" id="username2" class="input-normal" value="'.$username.'" required />
-	<label for="password2">'.Trad::F_PASSWORD.'</label>
-	<input type="password" name="password" id="password2" class="input-normal" required />
-	<label for="email">'.Trad::F_EMAIL.'</label>
-	<input type="email" name="email" id="email" class="input-large" value="'.$email.'" />
-	<p class="help">'.Trad::F_TIP_USER_EMAIL.'</p>
+	<label for="username2">Plex '.Trad::F_USERNAME.'</label>
+	<input type="text" name="username" id="username2" class="input-normal" value="'.$User->getUsername().'" readonly/>
 	<div class="form-actions">
 		<input type="hidden" name="token" value="'.getToken().'" />
 		<input type="hidden" name="new_user" value="1" />
